56e476c5d226f587d8fbaeb42bc461740e557c6e
TATSUYA <tatsuya63177136@gmail.com>
2020-07-22 22:50:15 +0900

    add superior to UserModel and edit seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_14_140843) do
+ActiveRecord::Schema.define(version: 2020_07_22_102002) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_07_14_140843) do
     t.string "department"
     t.datetime "basic_time", default: "2019-02-19 22:30:00"
     t.datetime "work_time", default: "2019-02-19 23:00:00"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,10 +3,21 @@ User.create(name: "管理者",
             password: "password",
             password_confirmation: "password",
             admin: true)
+4.times do |n|
+  name = Faker::Japanese::Name.name
+  email = "email#{n+1}@sample.com"
+  password = "password"
+  User.create!(name: name,
+               email: email,
+               password: password, 
+               password_confirmation: password,
+               superior: true)
+end
+
 
 59.times do |n|
   name = Faker::Japanese::Name.name
-  email = "email#{n+1}@sample.com"
+  email = "email#{n+5}@sample.com"
   password = "password"
   User.create!(name: name,
                email: email,
