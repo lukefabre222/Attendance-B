0cf3d7a73e5c5d6b41baed769f892a59e161cdc1
TATSUYA <tatsuya63177136@gmail.com>
2020-11-14 23:17:40 +0900

    edit attendance_mark

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   end
 
   def self.updatable_attributes
-    [ "name", "email", "department", "employee_number", "uid", "basic_time", "designated_work_start_time",
+    [ "name", "email", "department", "employee_number", "uid", "basic_time", "designated_work _start_time",
       "designated_work_end_time", "superior", "admin", "password"]
   end
 end
