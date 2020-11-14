414acb6d1980143f93f7343ca219d840fd8ceff0
TATSUYA <tatsuya63177136@gmail.com>
2020-11-14 22:51:24 +0900

    edit user_show

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   end
 
   def self.updatable_attributes
-    [ "name", "email", "department", "employee_number", "uid", "basic_time", "designated_worke_start_time",
+    [ "name", "email", "department", "employee_number", "uid", "basic_time", "designated_work_start_time",
       "designated_work_end_time", "superior", "admin", "password"]
   end
 end
