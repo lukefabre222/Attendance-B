62c2972aab6324f00ac06370911009d6b52614af
TATSUYA <tatsuya63177136@gmail.com>
2020-11-28 22:41:01 +0900

    check logic file &update

--- app/models/office.rb
+++ app/models/office.rb
@@ -2,5 +2,5 @@ class Office < ApplicationRecord
   validates :office_name, presence: true
   validates :office_number, uniqueness: true, 
                             numericality: { only_integer: true,
-                                            greater_than: 1, less_than: 100}
+                                            greater_than: 0, less_than: 100}
 end
